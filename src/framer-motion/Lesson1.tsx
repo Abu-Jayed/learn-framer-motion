9555399ebb1f9dd8b9d9be67e3e7f711043ea11f
Abu Jayed <aj.webdevlopment@gmail.com>
2024-01-29 19:52:38 +0600

    first commit

--- src/framer-motion/Lesson1.tsx
+++ src/framer-motion/Lesson1.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Lesson1 = () => {
   return (
     <div>
-      
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci vitae sunt culpa dolores consequatur, nostrum impedit repellendus. Maiores aliquid ullam id placeat, ea, in porro corrupti inventore unde ipsam autem.
     </div>
   );
 };
