145e563a0cc6c18da2c286a25e9b5fa1499b91cd
Abu Jayed <aj.webdevlopment@gmail.com>
2024-01-29 20:42:18 +0600

    lesson 2 complete.

--- src/framer-motion/Lesson2.tsx
+++ src/framer-motion/Lesson2.tsx
@@ -5,15 +5,36 @@ const Lesson2 = () => {
     hidden: { opacity: 0, scale: 0.9 },
     visible: { opacity: 1, scale: 1 },
   };
+  const child = {
+    hidden: { opacity: 0, scale: 0.5 },
+    visible: { opacity: 1, scale: 1 },
+  };
 
   return (
     <motion.div
       variants={parent}
       initial={"hidden"}
       animate={"visible"}
-      transition={{ duration: 4, repeat: Infinity }}
-      className="size-64 bg-emerald-500 rounded-xl"
-    ></motion.div>
+      transition={{ duration: 8, repeat: Infinity, delayChildren: 2,staggerChildren:1 }}
+      className="size-64 bg-emerald-500 rounded-xl flex gap-5 flex-wrap p-5 items-center justify-center"
+    >
+      <motion.div
+        variants={child}
+        className="size-20 bg-cyan-600 rounded"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="size-20 bg-cyan-600 rounded"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="size-20 bg-cyan-600 rounded"
+      ></motion.div>
+      <motion.div
+        variants={child}
+        className="size-20 bg-cyan-600 rounded"
+      ></motion.div>
+    </motion.div>
   );
 };
 
